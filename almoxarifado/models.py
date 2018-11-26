cbc01fa0ac09f84f12ad87a99b3c23dd6ccf8061
bren1n <brenoleite1234@gmail.com>
2018-11-26 15:21:54 -0300

    adição de pk autoimplementada

--- almoxarifado/models.py
+++ almoxarifado/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Almoxarifado(models.Model):
+    id = models.AutoField(primary_key = True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
