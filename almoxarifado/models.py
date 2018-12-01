ba9ff141d62ee9ab7f57aa91218fa80e083c5a6d
bren1n <brenoleite1234@gmail.com>
2018-12-01 17:01:13 -0300

    alteração de estilo do form.html

--- almoxarifado/models.py
+++ almoxarifado/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Almoxarifado(models.Model):
     id = models.AutoField(primary_key = True)
     nome = models.CharField(max_length=100)
-    descrição = models.CharField(max_length=100)
+    descrição = models.TextField()
     quantidade = models.PositiveIntegerField()
     usuário = models.CharField(max_length=100)
     
